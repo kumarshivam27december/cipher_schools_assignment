5fef479df572d2839944d611a9d1ac38d3b049bb
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-10-07 15:04:15 +0530

    version 4.0

---
+++ lec3_cipherschools.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=2;
+    bool b=1;
+    float c=2.0;
+    string d="string";
+    double e=2.00000;
+    cout<<a<<b<<c<<d<<e;
+          return 0;
+}
