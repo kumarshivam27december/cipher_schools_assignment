d5ff36431423e1753e7cd21b42f7ba1f0ac32dde
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2023-10-19 01:03:04 +0530

    Add files via upload

---
+++ lec8_part1_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// Function declaration
+void displayMessage();
+
+int main() {
+    // Function call
+    displayMessage();
+    
+    return 0;
+}
+
+// Function definition
+void displayMessage() {
+    cout << "Hello, World!" << std::endl;
+}
